1632a25230dc52c753673201ffe90f3e6dc5dc8e
Shaun Luttin <shaunluttin@builddirect.com>
2017-03-01 12:01:02 -0800

    Add QueueBackgroundWorkItem example

---
+++ QueueBackgroundWorkItem/WebApplication1/Global.asax.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+using System.IO;
+
+namespace WebApplication1
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        private const long Iterations = long.MaxValue;
+
+        private const string ClassString = "Class-";
+
+        private string ObjectString = "Object-";
+
+        protected void Application_Start()
+        {
+            RouteTable.Routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            HostingEnvironment.QueueBackgroundWorkItem(ct => DoWorkAsync("Method-"));
+        }
+
+        private async Task DoWorkAsync(string methodString)
+        {
+            await Task.Run(() =>
+            {
+                var appData = HttpRuntime.AppDomainAppPath;
+                var path = Path.Combine(appData, "foo.txt");
+                File.Decrypt(path);
+                for (long i = 0; i < Iterations; i = i + 1)
+                {
+                    File.AppendAllText(path, ClassString + ObjectString + methodString + i.ToString() + Environment.NewLine);
+                }
+            });
+        }
+    }
+}
